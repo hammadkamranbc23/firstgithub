a331bd73bc356fd04cd5aa42de90bf7e1a7afa7c
Hammad Kamran <hammadkamran999@gmail.com>
2022-11-10 12:19:58 +0500

    3rd commit

---
+++ Jupyter_notebook/Untitled-1.py
@@ -0,0 +1,38 @@
+# %%
+# body mass index (BMI)
+# input Weight
+# Height
+# BMI (Ask yourself your height)
+# Weight
+# Calculate BMI
+# print BMI (My name is_____, My BMI is -----)
+
+# %%
+# weight in Kg/height in m and its square
+
+# %%
+height = input("WHat is your height? ")
+
+# %%
+height = float(height)
+
+# %%
+weight = input("What is your weight? ")
+
+# %%
+weight = float(weight)
+
+# %%
+name = input("What is your name? ")
+
+# %%
+BMI = weight/height**2
+BMI
+
+# %%
+print("My name is", name, "and My BMI is", BMI)
+
+# %%
+pip install seaborn
+
+
